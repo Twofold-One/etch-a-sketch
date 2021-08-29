9f7923dd9cc801da1e14806bc2440fe3e830f31f
Twofold-One <evan_geras@mail.ru>
2021-08-30 00:29:28 +0700

    added RESET button, with user grid size option

--- scripts/main.js
+++ scripts/main.js
@@ -16,26 +16,76 @@ function multiDivGrid() {
     for (i = 0; i < 256; i++) {
         divGrid();
     }
-
 }
 
 multiDivGrid();
+
+//function for RESET button
+function resetAndAlign() {
+    const resetButton = document.querySelector('#reset-button');
+    
+    //func for reseting the grid
+    function gridReset() {
+        const gridElement = document.querySelectorAll('#div-element');
+        gridElement.forEach((element) => {
+            element.style.background = 'white';
+        });
+    }
+
+    //func for user prompt for grid align
+    function gridAlignPrompt() {
+        const userGridPrompt = prompt('What number of squares per side for the new grid do you want?');
+
+
+        //userMultiDivGrid
+        function userMultiDivGrid(userGrid) {
+            const divContainer = document.querySelector('#container');
+
+            //func delete old grid
+            function deleteOldGrid() {
+                while (divContainer.hasChildNodes()) {
+                    divContainer.removeChild(divContainer.firstChild);
+                }
+            }
+            //func creates one div element in container
+            function divGrid() {
+                const divElement = document.createElement('div');
+                divElement.setAttribute('id', 'div-element');
+                divElement.classList.add('div-element');
+        
+                divContainer.appendChild(divElement);
+            }
+            
+            deleteOldGrid();
+
+            for (i = 0; i < userGrid**2; i++) {
+                divGrid();
+            }
+            divContainer.setAttribute(`style`,
+            `grid-template-columns: repeat(${userGridPrompt}, 1fr);
+            grid-template-rows: repeat(${userGridPrompt}, 1fr);` );
+        }
+       
+        userMultiDivGrid(userGridPrompt);
+    }
+
+    resetButton.addEventListener('click', function(e) {
+        gridReset();
+        gridAlignPrompt();
+        divHoverColor();
+    });
+}
+
+resetAndAlign();
 
 //func change color of div element on hover
 function divHoverColor() {
     const divElement = document.querySelectorAll('#div-element');
 
     divElement.forEach((element) => {
-
-        //func changes the color of div
-        function changeDivColor() {
-        divElementColor = document.querySelector('#div-element');
-        
-        divElementColor.classList.remove('div-element');
-        divElementColor.classList.add('div-element-mouseover');
-    }
-
-        element.addEventListener('click', changeDivColor());
+        element.addEventListener('mouseover', function(e) {
+            e.target.style.background = 'black';
+        });
     });
 }
 
